f8708aeaaf27d57338cab77d9a76999cac3cd089
vikash kumar <vikashkumarkeshari9090@gmail.com>
2024-08-02 07:57:53 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
+// import app from "./index.js";
+
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server Listening At Port ${process.env.PORT}`);
+// });
+
+
+
 import app from "./index.js";
 
+// Define the port variable with a default value
+const port = process.env.PORT || 4001; // Use port 4001 as the default if not set
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server Listening At Port ${process.env.PORT}`);
+app.listen(port, () => {
+  console.log(`Server Listening At Port ${port}`);
 });
